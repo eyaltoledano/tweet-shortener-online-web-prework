f0154b0bfce5bf7958d06d85c13577c2b5f260f4
Eyal Toledano <eyaltoledano@me.com>
2019-02-07 03:41:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -50,8 +50,9 @@ end
 
 def shortened_tweet_truncator(tweet)
   truncated_tweet = []
+  truncated_part = tweet[0..136]
   if tweet.length > 140
-    truncated_tweet << tweet.[0..136]
+    truncated_tweet << tweet[0..136]
     truncated_tweet << " (...)"
   end
 end
